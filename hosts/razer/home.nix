8bc4c7812243751162dae37c4a1bf50aeaa10695
roshan <roshanahegde@gmail.com>
2024-08-15 14:08:54 -0500

    tidying up home.nix

--- hosts/razer/home.nix
+++ hosts/razer/home.nix
@@ -7,31 +7,18 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-	  ../../modules/home-manager/hyprland
-	  ../../modules/home-manager/nvim
-      ../../modules/home-manager/alacritty
-      ../../modules/home-manager/fish.nix
-  ];
+  imports = let
+      home-manager = [ 
+        "hyprland" 
+        "nvim" 
+        "alacritty" 
+        "fish.nix"
+        "firefox.nix"
+      ];
+  in map (module: ../../modules/home-manager + module) home-manager;
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
         inputs.prismlauncher.overlays.default
     ];
     # Configure your nixpkgs instance
