2a277fab115e887969786ef74dbecf6238fc4b5c
roshan <roshanahegde@gmail.com>
2026-05-21 10:04:06 -0500

    openssh module

---
+++ modules/nixos/openssh.nix
@@ -0,0 +1,29 @@
+{
+  outputs,
+  lib,
+  config,
+  ...
+}: {
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Harden
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+
+      # Automatically remove stale sockets
+      StreamLocalBindUnlink = "yes";
+      # Allow forwarding ports to everywhere
+      GatewayPorts = "clientspecified";
+      # Let WAYLAND_DISPLAY be forwarded
+      AcceptEnv = ["WAYLAND_DISPLAY"];
+      X11Forwarding = true;
+    };
+  };
+
+  # Passwordless sudo when SSH'ing with keys
+  security.pam.sshAgentAuth = {
+    enable = true;
+    authorizedKeysFiles = ["/etc/ssh/authorized_keys.d/%u"];
+  };
+}
